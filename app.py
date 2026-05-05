028dae66ec5e45758c3ec64fa26abf90942cdabe
Mobarak T <mobarakt@Mobaraks-Mac-mini.local>
2026-05-05 10:40:24 +0300

    fix render port

--- app.py
+++ app.py
@@ -314,5 +314,10 @@ def too_large(_):
     return redirect(url_for("upload"))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+    
